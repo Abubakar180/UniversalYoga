759fc57eba4f6bb5c27ca7c4c720520888655a3e
Abubakar180 <abubakar.tariq.180@gmail.com>
2024-03-27 00:53:27 +0500

    Comments Added

--- Views/BookedPage.xaml.cs
+++ Views/BookedPage.xaml.cs
@@ -6,6 +6,7 @@ namespace UniversalYoga.Views;
 
 public partial class BookedPage : ContentPage
 {
+    //This is Backend of BookedPage, it's Binding context setted with BookedViewModel
 	public BookedViewModel vm;
 
     public BookedPage()
@@ -13,12 +14,14 @@ public partial class BookedPage : ContentPage
 		InitializeComponent();
 		BindingContext = vm = new BookedViewModel();
     }
+    //OnAppearing calls every time this page appears and runs the method implemented in BookedViewModel.
     protected override void OnAppearing()
     {
         base.OnAppearing();
         vm.courses = new ObservableCollection<YogaCourse>();
         vm.GetAllCourses();
     }
+    //Entry Text Changed Event by searching courses by name.
     private void search_TextChanged(object sender, TextChangedEventArgs e)
     {
         Device.BeginInvokeOnMainThread(() =>
